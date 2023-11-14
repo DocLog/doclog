f1021711dec35a91eda538df70d838c9781c65bc
Carlos Santos <cadu.vs@hotmail.com>
2023-11-13 22:28:52 -0300

    mock: mocked db setup

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from api import create_app
 
 app = create_app()
 
+# setup database
 
+from api.common import db
+from tests.integration.mock import generate_fake_data
+
+with app.app_context():
+    db.create_all()
+    generate_fake_data()
+    
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
